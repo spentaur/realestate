43bae368c3ba6150693ccdb8c3b59ab0571874d4
Spencer Adams <hi@spentaur.com>
2021-04-15 22:09:30 -0500

    go back to keys?

--- pages/contact.vue
+++ pages/contact.vue
@@ -13,7 +13,7 @@
                   <img
                     v-show="loaded"
                     class="object-cover w-full h-full"
-                    src="~/assets/img/downtown-urbana.jpg"
+                    src="~/assets/img/keys.jpg"
                     alt="Handing Keys to Someone"
                     @load="onLoaded"
                   />
