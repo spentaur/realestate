3fbc2d61e64052f662fd6b46f4196533ef24207d
Spencer Adams <hi@spentaur.com>
2021-05-11 22:42:58 -0500

    refactor(buy): change size of close button

--- pages/buy.vue
+++ pages/buy.vue
@@ -12,7 +12,7 @@
             <!-- Heroicon name: outline/x -->
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="w-6 h-6"
+              class="w-6 h-6 text-gray-600 md:w-8 md:h-8"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
