fccbb0989f88a887b93c19781cd9f9125ba07b13
Spencer Adams <hi@spentaur.com>
2021-05-13 09:49:20 -0500

    refactor(buy): changing stuff on buy page

--- pages/buy.vue
+++ pages/buy.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="flex flex-col bg-white">
     <div class="p-2 sm:p-3">
-      <div class="flex flex-wrap items-center justify-end">
+      <div class="flex flex-wrap items-center justify-start">
         <div class="flex-shrink-0 order-2 sm:order-3 sm:ml-2">
           <button
             type="button"
-            class="flex p-2 -mr-1 focus:outline-none focus:ring-2 focus:ring-white"
+            class="flex items-center justify-center p-2 -mr-1 focus:outline-none focus:ring-2 focus:ring-white"
             @click.prevent="close"
           >
             <span class="sr-only">Dismiss</span>
             <!-- Heroicon name: outline/x -->
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="w-6 h-6 text-gray-600 md:w-8 md:h-8"
+              class="w-6 h-6 text-indigo-400"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -21,7 +21,7 @@
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
               />
             </svg>
           </button>
