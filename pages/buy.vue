051debc6f977e31e43d4bb84549de9c7b347d6e9
Spencer Adams <hi@spentaur.com>
2021-05-17 17:58:36 -0500

    refactor(buy): idk lol

--- pages/buy.vue
+++ pages/buy.vue
@@ -12,7 +12,7 @@
             <!-- Heroicon name: outline/x -->
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="w-6 h-6 text-indigo-400"
+              class="w-6 h-6 text-gray-600"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
